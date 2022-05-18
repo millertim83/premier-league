a90ebb0e374cd89b843505aff28dff53f97dc9d0
millertim83 <80060826+millertim83@users.noreply.github.com>
2022-05-18 15:00:07 -0400

    began setting up table

--- src/Components/LeagueTable.js
+++ src/Components/LeagueTable.js
@@ -2,8 +2,20 @@ import React from "react";
 
 function LeagueTable() {
     return (
-        <div>
-            
+        <div className="table table-bordered">
+            <thead>
+                <tr>
+                    <th>Team</th>
+                    <th>Played</th>
+                    <th>Wins</th>
+                    <th>Draws</th>
+                    <th>Losses</th>
+                    <th>Goals For</th>
+                    <th>Goals Against</th>
+                    <th>Points</th>
+                </tr>
+            </thead>
+
         </div>
     )
 }
