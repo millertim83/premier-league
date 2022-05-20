d995bba97d44893a23b29ea5791a10169d817184
millertim83 <80060826+millertim83@users.noreply.github.com>
2022-05-20 11:29:12 -0400

    implemented basic NavBar.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css"
 import Home from "./Components/home/Home.js";
+import NavBar from "./Components/NavBar";
 
 
 function App() {
   return (
     <div>
+      <NavBar />
       <Home />
     </div>
   );
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -3,9 +3,28 @@ import React from "react";
 function NavBar() {
     return (
         <div>
-            <nav className="navbar ">
-                
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+	            <a className="navbar-brand" href="#">Navbar</a>
+	            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    	            <span className="navbar-toggler-icon"></span>
+  	            </button>
+
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Weekly Results</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Scoring Leaders</a>
+                        </li>
+                    </ul>
+                </div>
             </nav>
         </div>
     )
 }
+
+export default NavBar;
