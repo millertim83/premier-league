1dd0c8959d1372f0b0903598ad5438624d3ec8f3
millertim83 <80060826+millertim83@users.noreply.github.com>
2022-05-23 15:26:16 -0400

    installed react router for multi-page app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css"
-//import Home from "./Components/home/Home.js";
+import Home from "./Components/home/Home.js";
 import NavBar from "./Components/NavBar";
-//import WeeklyResults from "./Components/WeeklyResults/WeeklyResults.js";
+import WeeklyResults from "./Components/WeeklyResults/WeeklyResults.js";
 import ScoringLeaders from "./Components/scoringLeaders/ScoringLeaders";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 
 function App() {
   return (
-    <div>
-      <NavBar />
-      <ScoringLeaders />
-    </div>
+    <Router>
+      <div className="App">
+        <NavBar />
+        <div className="content">
+          <Switch>
+            <Route path="/" exact component = {() => <Home />} />
+            <Route path="/weeklyresults" exact component = {() => <WeeklyResults />} />
+            <Route path="/scoringleaders" exact component = {() => <ScoringLeaders />} />
+          </Switch>
+          
+        </div>
+      </div>
+    </Router>
   );
 }
 
